bbb38ddb1c2d6a8cd39bc394243b67d456eb19ee
ErickApps <pitjitsu@hotmail.com>
2016-06-02 23:10:32 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using homework3.Models;
+
+namespace homework3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table(int number)
+        {
+             var num = new Model() {Num = number};
+                
+            return View(num);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace homework3.Models
+{
+    public class Model
+    {
+        public int Num { get; set; }
+        
+
+        public static List<int> multiplication(int number)
+        {
+            List<int> list = new List<int>();
+            int counter = number;
+            number = number*number;
+            int result;
+
+            for (int i = 1; i <= counter; i++)
+            {
+
+                for (int j = 1; j <= counter; j++)
+                {
+
+                    result = j*i;
+                    list.Add(result);
+                }
+
+                
+            }
+            return list;
+        }
+    }
+}
